a00e2fd934ee7d23fc8512e166c87e193d43ba9c
shadhin <shadhinahmed449@gamil.com>
2024-08-31 13:52:50 +0600

    All done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,13 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+   }
+
+    buildFeatures {
+        viewBinding=true
     }
+
+
 }
 
 dependencies {
--- app/src/main/java/com/cscomer/bmi/MainActivity.kt
+++ app/src/main/java/com/cscomer/bmi/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cscomer.bmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottom.setOnClickListener {
+
+            val height=binding.hight.text.toString().toDouble()/100
+            val weight=binding.wight.text.toString().toDouble()
+
+
+            val bmi=weight/(height*height)
+
+
+            binding.bmiTV.text= String.format("BMI is : %.2f",bmi)
+
+
+           binding.result.text=  when(bmi){
+                in 0.0..12.1 -> "BMI Type is : Under low"
+                in 12.2..20.2 -> "BMI type is :Normal "
+                in 20.2..30.1->"BMI Type is : Hight"
+                in 30.2..40.3-> "BMI Type is : overHight"
+
+                else-> "BMi Type is :Obesity "
+
+
+            }
+
         }
+
     }
 }
